afe4736fe3c3841c28ed1e31d1b52f2d37942ef9
NicolasRyjou <71492089+NicolasRyjou@users.noreply.github.com>
2022-01-15 13:36:32 +0100

    Finalizing. Added google maps features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,23 +34,22 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+// import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+// import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+// import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+// import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+// import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+// import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+// import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+// import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+// import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+// import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+// import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+// import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+// import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+// import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+// import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatIconModule } from '@angular/material/icon';
 import { AgmCoreModule } from '@agm/core';
 
@@ -80,7 +79,7 @@ const imports = [
 ]
 
 @NgModule({
-  imports: [...imports, MdbAccordionModule, MdbCarouselModule, MdbCheckboxModule, MdbCollapseModule, MdbDropdownModule, MdbFormsModule, MdbModalModule, MdbPopoverModule, MdbRadioModule, MdbRangeModule, MdbRippleModule, MdbScrollspyModule, MdbTabsModule, MdbTooltipModule, MdbValidationModule, BrowserAnimationsModule, NgbModule],    
+  imports: [...imports], //, MdbAccordionModule, MdbCarouselModule, MdbCheckboxModule, MdbCollapseModule, MdbDropdownModule, MdbFormsModule, MdbModalModule, MdbPopoverModule, MdbRadioModule, MdbRangeModule, MdbRippleModule, MdbScrollspyModule, MdbTabsModule, MdbTooltipModule, MdbValidationModule, BrowserAnimationsModule, NgbModule],    
   providers: [
     {
       provide: 'SocialAuthServiceConfig',
--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -1,37 +1,126 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core'
-import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps'
+import { Component, Input, OnInit } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
 import { ChatClass } from 'src/app/structures/chat-d-struc';
+
+
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+  draggable: boolean;
+  content?:string;
+  isShown:boolean;
+  icon:string;
+}
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
-  styleUrls: ['./maps.component.css'],
+  styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
-  @ViewChild(GoogleMap, { static: false }) map: GoogleMap
-  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow
 
-  @Input() markers: ChatClass[] = [];
-  @Input() radiusOfChats = 0;
-  center;
-  lat = 51.678418;
-  lng = 7.809007;
+  title: string = 'AGM project';
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  private geoCoder;
 
+  // Radius
+  @Input() radiusOfChats = 50000;
+  radiusLat = 0;
+  radiusLong = 0;
+ 
+  @Input() closeChats: ChatClass[] = [];
+
+  markers: marker[] = []
+ 
+  constructor(
+    private mapsAPILoader: MapsAPILoader
+  ) {
+ 
+  }
+ 
+ 
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      }
-    })
-    
+    //load Map
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+    });
+  }
+  onMapReady(map?: google.maps.Map ){
+    if(map)
+      map.setOptions({
+        streetViewControl: false
+      });
+  }
+ 
+  // Get Current Location Coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.radiusLat = this.latitude;
+        this.radiusLong = this.longitude;
+        this.zoom = 6;
+       
+        for(let i=1;i<50;i++){
+          this.markers.push(
+            {
+              lat: this.latitude+Math.random(),
+              lng: this.longitude+Math.random(),
+              label: `${i}`,
+              draggable: false,
+              content: `Content no ${i}`,
+              isShown:false,
+              icon:'./assets/marker-red.png'
+            }
+          );
+        }
+
+      });
+    }
   }
 
-  logCenter() {
-    console.log(JSON.stringify(this.map.getCenter()))
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
   }
 
-  openInfo(marker: MapMarker, content) {
-    this.info.open(marker)
+  radiusDragEnd($event: any) {
+    console.log($event);
+    this.radiusLat = $event.coords.lat;
+    this.radiusLong = $event.coords.lng;
+    this.showHideMarkers();
   }
+
+  event(type,$event) {
+    console.log(type,$event);
+    this.radiusOfChats = $event;
+    this.showHideMarkers();
+  }
+
+  showHideMarkers(){
+    Object.values(this.markers).forEach(value => {
+      value.isShown = this.getDistanceBetween(value.lat,value.lng,this.radiusLat,this.radiusLong);
+    });
+  }
+
+  getDistanceBetween(lat1,long1,lat2,long2){
+    var from = new google.maps.LatLng(lat1,long1);
+    var to = new google.maps.LatLng(lat2,long2);
+
+    if(google.maps.geometry.spherical.computeDistanceBetween(from,to) <= this.radiusOfChats){    
+      console.log('Radius',this.radiusOfChats);
+      console.log('Distance Between',google.maps.geometry.spherical.computeDistanceBetween(
+        from,to
+      ));
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
 }
